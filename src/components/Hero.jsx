1ab31106a8ae599d89dc57e05a3d3b53f97931a5
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-03-20 13:49:18 +0000

    landing page complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,13 @@ import SearchBar from "./SearchBar";
 const Hero = () => {
   return (
     <section className="hero-container">
-      <div>
-        <h1>Empowering your mind, transforming your life.</h1>
-        <p>Your mental health matters, let's work on it together.</p>
-      </div>
-      <SearchBar />
+      <section className="hero-info">
+        <div>
+          <h1>Empowering your mind, transforming your life.</h1>
+          <p>Your mental health matters, let's work on it together.</p>
+        </div>
+        <SearchBar />
+      </section>
     </section>
   );
 };
