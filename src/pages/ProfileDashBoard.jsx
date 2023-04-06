032b0be9c3ab9c48c9beb1dc0a6aef05a7ebbc01
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-04-06 14:43:12 +0000

    adding first section of dashboard layout

--- src/pages/ProfileDashBoard.jsx
+++ src/pages/ProfileDashBoard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import profpic from "../images/pic.png";
+import coverpic from "../images/CoverPhoto.png";
+import "../assets/ProfileDashBoard.css";
 
 const ProfileDashBoard = () => {
   return (
@@ -14,7 +17,25 @@ const ProfileDashBoard = () => {
           <p className="name">Grace Ashley</p>
         </div>
       </nav>
-      <section>Profile</section>
+      <div className="coverpic">
+        <img src={coverpic} id="coverpic" alt="coverpic" />
+      </div>
+      <section className="dashboard-body">
+        <section className="person-info">
+          <div className="sub-1">
+            <img src={profpic} alt="profilepic" />
+            <div className="sub-2">
+              <h3>Grace Antwi</h3>
+              <div className="person-title">
+                <p className="ash">Psychotherapy, LCSW </p>
+                <p className="green">She/Her</p>
+              </div>
+            </div>
+          </div>
+          <button className="edit-btn">Edit Profile</button>
+        </section>
+        <div className="person-bio"></div>
+      </section>
     </main>
   );
 };
